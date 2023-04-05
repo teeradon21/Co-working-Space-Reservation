eb19b6fdf0824d9bfc99e7227f081bfd753eed41
teeradon21 <teeradon21@outlook.com>
2023-04-05 21:24:03 +0700

    initial code

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+const connectDB = require('./config/db');
+
+//Load env vars
+dotenv.config({path:'./config/config.env'});
+
+//connect to database
+connectDB();
+
+//Route files
+const hospitals = require('./routes/hospitals');
+const auth = require('./routes/auth');
+const appointments = require('./routes/appointments');
+
+const app=express();
+
+//Body parser
+app.use(express.json());
+
+//Cookie parser
+app.use(cookieParser());
+
+app.use('/api/v1/hospitals',hospitals);
+app.use('/api/v1/auth',auth);
+app.use('/api/v1/appointments',appointments);
+
+// app.get('/',(req,res) => {
+//     res.status(200).json({success:"true", data:{id:1}});
+// })
+
+
+const PORT=process.env.PORT || 5000;
+
+const server = app.listen(PORT,console.log('Server running in ', process.env.NODE_ENV, ' mode on port ',PORT));
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection',(err,promise)=>{
+    console.log(`Error: ${err.massage}`);
+    //Close server & exit process
+    server.close(()=>process.exit(1));
+});
